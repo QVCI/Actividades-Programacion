b4cf22f78263c8bb18f72b302be910f25bae369e
QVCL <quezada.venegas.carloivan@gmail.com>
2025-04-17 23:11:57 -0600

    feat Inicializacion Matrices y validaciones

--- Primer Semestre/Fundamentos de Programación/Matrices.c
+++ Primer Semestre/Fundamentos de Programación/Matrices.c
@@ -21,6 +21,11 @@ void InicializarMatricesDefecto();
 //Generar menu Principal
 void MenuPrincipal();
 
+//Validar Input
+bool ValidarInputMain(char);
+
+
+
 //declaracion variables
 int i;
 int j;
@@ -32,7 +37,7 @@ int MatrizC [3][3];
 
 
 //Inicio Script
-int Main()
+int main()
 {
 	srand(time(NULL));
 	InicializarMatricesDefecto();
@@ -55,7 +60,6 @@ void Esperar()
     int cerrar;
     printf("Presione cualquier numero para continuar\n");
     scanf("%d", &cerrar);
-    LimpiarPantalla();
 }
 
 void InicializarMatricesDefecto()
@@ -72,5 +76,90 @@ void InicializarMatricesDefecto()
 }
 void MenuPrincipal()
 {
+  
+    char input;
+    bool inputvalido = true;
+    do
+    {
+        if (inputvalido)
+        {
+            LimpiarPantalla();
+            printf("Menu Principal\n");
+            printf("1. Inicializar Matriz A.\n");
+            printf("2. Inicializar Matriz B.\n");
+            printf("3. Suma (Matriz C = Matriz A + Matriz B).\n");
+            printf("4. Resta (Matriz C = Matriz A - Matriz B).\n");
+            printf("5. Multiplicacion (Matriz C = Matriz A * Matriz B).\n");
+            printf("6. Salir.\n");
+            printf("Seleccione una opcion:\n");
+        }
+        else
+        {
+            LimpiarPantalla();
+            printf("Menu Principal\n");
+            printf("1. Inicializar Matriz A.\n");
+            printf("2. Inicializar Matriz B.\n");
+            printf("3. Suma (Matriz C = Matriz A + Matriz B).\n");
+            printf("4. Resta (Matriz C = Matriz A - Matriz B).\n");
+            printf("5. Multiplicacion (Matriz C = Matriz A * Matriz B).\n");
+            printf("6. Salir.\n");
+            printf("Seleccione una opcion:\n");
+            printf("Recuerde que solo se permiten numeros entre 1 y 6.\n");
+        }
+        scanf(" %c", &input);
+        inputvalido = ValidarInputMain(input);
+        switch (input)
+        {
+            case '1':
+            //Inicializar Matriz A
+            LimpiarPantalla();
+            printf("Inicializando Matriz A...\n");
+            printf("Matriz A:\n");
+            for(i = 0; i <= 2; ++i)
+            {
+                for(j = 0; j <= 2; ++j)
+                {
+                    int numRandom = NumerosAleatorios(1,3);
+                    MatrizA[i][j] = numRandom;
+                    printf("%d  ", numRandom);
+                }
+                printf("\n");
+            }
+            Esperar();
 
+            case '2':
+            //Inicializar Matriz B
+            LimpiarPantalla();
+            printf("Inicializando Matriz B...\n");
+            printf("Matriz B:\n");
+            for(i = 0; i <= 2; ++i)
+            {
+                for(j = 0; j <= 2; ++j)
+                {
+                    int numRandom = NumerosAleatorios(1,3);
+                    MatrizB[i][j] = numRandom;
+                    printf("%d  ", numRandom);
+                }
+                printf("\n");
+            }
+            Esperar();
+
+        }
+            
+       
+         
+    } while (input != '6');
+    
+}
+
+bool ValidarInputMain(char input)
+{
+    if (input == '1' || input == '2' || input == '3' || input == '4' || input == '5' || input == '6')
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
