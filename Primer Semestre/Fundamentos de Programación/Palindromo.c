fb625fad95966d4dd77a42dc774572da8977cde0
QVCL <quezada.venegas.carloivan@gmail.com>
2025-05-13 19:41:45 -0600

    feat MenuPrincipal con validaciones

--- Primer Semestre/Fundamentos de Programación/Palindromo.c
+++ Primer Semestre/Fundamentos de Programación/Palindromo.c
@@ -10,15 +10,21 @@
 //Limpia pantalla
 void LimpiarPantalla();
 
+//Menu Principal
+void MenuPrincipal(int);
+
+//Validacion del menu
+int Validacion(char[]);
+
 //Esperar
 void Esperar();
 
 
 //declaracion variables
-int vocales;
-int espacios;
-
-char texto [80];
+int bandera = 0;
+int i, codigo_error = 8;
+char texto[100];
+char opcmenu[20];
 
 
 
@@ -26,18 +32,79 @@ char texto [80];
 //Inicio Script
 int main()
 {
-    while(true)
+    do
     {
         LimpiarPantalla();
-        printf("Ingrese un texto para validar si es palindromo\n");
-        fget 
-        scanf("%s", texto);
+        MenuPrincipal(codigo_error);
+        scanf("%s", &opcmenu);
+        codigo_error = Validacion(opcmenu);
         
-        
-
-    }
+    } while (true);
+    
+    
 
 	return 0;
+}
+int Validacion(char opcmenu[20])
+{
+    if(strlen(opcmenu) > 1)
+    {
+        return 3; //codigo de error para mas de un digito
+    }
+   
+    for(i =0; i < strlen(opcmenu); i++)
+    {
+        if(!isdigit(opcmenu[i]))
+        {
+            return 4; //código de error para no numerico;
+        }       
+    }
+    for(i =0; i < strlen(opcmenu); i++)
+    {
+        if(opcmenu[i] != '0' || opcmenu[i] != '1' || opcmenu[i] != '2')
+        {
+            
+            return 5; //codigo de error para menu incorrecto
+        }
+        else
+        {
+            switch (opcmenu[i])
+            {
+                case '0':
+                    return 0; //Opción para salir
+                    break;
+                case '1':
+                    return 1; //Opcion palindromo
+                    break;
+                case '2':
+                    return 2; //Opcion bubble sort
+                    break;
+            }
+        }    
+    }
+
+}
+void MenuPrincipal(int codigo_error)
+{
+    printf("Bienvenido al programa de palindromos\n");
+    printf("0. Salir\n");
+    printf("1. Palindromo\n");
+    printf("2. Bubble Sort\n");
+    printf("Seleccione una opcion: \n");
+    if(codigo_error == 3)
+    {
+        printf("La opcion seleccionada, debe tener longitud de 1 caracter\n");
+    }
+    if(codigo_error == 4)
+    {
+        printf("La opcion seleccionada, debe ser un numero\n");
+    }
+    if(codigo_error == 5)
+    {
+        printf("La opcion seleccionada, se debe encontrar entre 0 y 2\n");
+    }
+
+
 }
 void LimpiarPantalla()
 {
